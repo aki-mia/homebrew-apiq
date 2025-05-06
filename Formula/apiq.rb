bb6aac3ce698161daff8d100bddcc1b2df294ab6
aki-mia <13302223+aki-mia@users.noreply.github.com>
2025-05-06 16:01:06 +0900

    Update sha256 for v0.1.0

--- Formula/apiq.rb
+++ Formula/apiq.rb
@@ -2,15 +2,14 @@ class Apiq < Formula
   desc "A powerful and easy-to-use API CLI tool for developers"
   homepage "https://github.com/aki-mia/apiq"
   url "https://github.com/aki-mia/apiq/releases/download/v0.1.0/apiq-0.1.0.tar.gz"
-  sha256 "ここにshasum -a 256で得たハッシュを記載"
-  version "0.0.1"
+  sha256 "8bcec3e9e89fb0eb9c19612d3462e1c494f3d7fa4c8e5a928788b9d434b7fc6f"
+  version "0.1.0"
 
   depends_on "ruby"
 
   def install
     bin.install "bin/apiq"
     lib.install Dir["lib/*"]
-    prefix.install "config.yml"
   end
 
   test do
